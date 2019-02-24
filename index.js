b5084715495bad3c67f9d01fb41437bfc2fcd53a
aaronshivers <aaron@moonmail.org>
2019-02-24 10:16:57 -0600

    moved GET / to routes folder

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
 require('dotenv').config()
 
 const express = require('express')
+
+const index = require('./routes/index')
+
 const app = express()
 const { PORT } = process.env
 
-app.get('/', (req, res) => res.send('Ratings App'))
+app.use('/', index)
+
+app.use(function(err, req, res, next) {
+  console.error(err.message)
+  res.status(500).send('Something broke!')
+})
 
 app.listen(PORT, () => {
   console.log(`Server listening on port ${ PORT }.`)
--- tests/integration/index.test.js
+++ tests/integration/index.test.js
@@ -1,4 +1,5 @@
 const request = require('supertest')
+const expect = require('expect')
 const { app } = require('../../index')
 
 describe('index', () => {
