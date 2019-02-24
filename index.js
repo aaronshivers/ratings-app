18332c39a7241d64fbd4d34a44c39fdd398b5383
aaronshivers <aaron@moonmail.org>
2019-02-24 09:24:11 -0600

    added index.js

---
+++ index.js
@@ -0,0 +1,11 @@
+require('dotenv').config()
+
+const express = require('express')
+const app = express()
+const { PORT } = process.env
+
+app.get('/', (req, res) => res.send('Ratings App'))
+
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${ PORT }.`)
+})
